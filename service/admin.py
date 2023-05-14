9bb00c9d10c8b9f6bca938d6225a74f4ea5e0dc3
Ayush Gupta <gupta07ayush@gmail.com>
2023-05-14 20:47:58 +0530

    create services model fields, installed apps, mapped to admin

--- service/admin.py
+++ service/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+# import your Services model here
+from service.models import Services
+
+# list all fields of your Services models which you want to show in admin panel
+
+
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('service_icon', 'service_title', 'service_des')
+
+
 # Register your models here.
+admin.site.register(Services, ServiceAdmin)
--- service/models.py
+++ service/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+# creating services table in database
+
+
+class Services(models.Model):
+    service_icon = models.CharField(max_length=50)
+    service_title = models.CharField(max_length=50)
+    service_des = models.TextField()
